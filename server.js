465d14aa5df9abe1b2a7ab5ca82bf8c02a297971
Omar-Saad-Salim <froomomer66@gmail.com>
2025-10-27 13:26:47 +0300

    Deploy APP Version 1.1.1

--- server.js
+++ server.js
@@ -111,9 +111,9 @@ app.get('/api/update-check', (req, res) => {
       latestVersion: '1.1.1',
       updateType: 'app-update',
       appUpdate: {
-        downloadUrl: 'https://github.com/Omar1Saad/Grade-Management/releases/download/v1.1.1/%D9%86%D8%B8%D8%A7%D9%85%20%D8%A5%D8%AF%D8%A7%D8%B1%D8%A9%20%D8%AF%D8%B1%D8%AC%D8%A7%D8%AA%20%D8%A7%D9%84%D8%B7%D9%84%D8%A8%D8%A9-1.1.1.AppImage',
+        downloadUrl: 'https://github.com/Omar1Saad/Grade-Management/releases/download/v1.1.1/-1.1.1.AppImage',
         version: '1.1.1',
-        size: '117 MB',
+        size: '112 MB',
         features: [
           'إزالة زر الاختبار',
           'تحسينات في الأداء',
